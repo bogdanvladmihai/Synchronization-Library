f9fc87339563d536e4fa1c360ba495baf36e6e3b
Vlad-Mihai Bogdan <v.bogdan19@yahoo.com>
2024-01-16 18:50:38 +0200

    modified testcase for prod_cons

--- testing/producer_consumer_test.cpp
+++ testing/producer_consumer_test.cpp
@@ -6,33 +6,27 @@
 #include <numeric>
 
 int main() {
-  const int WORKERS = 1e5;
+  const int WORKERS = 10000;
   const int ITEMS = 5;
 
   ProducerConsumer <int> PS(ITEMS);
 
   std::vector<int> items;
-  std::vector<int> l(WORKERS);
-  // std::thread t([&PS, &items]() {
-  //        int item = PS.get_item();
-  //        items.push_back(item);
-  //      });
-  // std::thread t2([&PS]() {
-  //        PS.add_item(1);
-  //      });
-  // t.join();
-  // t2.join();
   std::vector<std::thread> threads;
-  std::iota(l.begin(), l.end(), 0);
-  for (int &i : l) {
+  Mutex items_mutex;
+
+  for (int i = 0; i < WORKERS; i++) {
     if (i % 2 == 1) {
-      threads.push_back(std::thread ([&PS, &i]() {
+      threads.push_back(std::thread ([&PS, i]() {
         PS.add_item(i);
       }));
     } else {
-      threads.push_back(std::thread ([&PS, i, &items]() {
+      threads.push_back(std::thread ([&PS, i, &items, &items_mutex]() {
         int item = PS.get_item();
+
+        items_mutex.lock();
         items.push_back(item);
+        items_mutex.unlock();
       }));
     }
   }
@@ -40,7 +34,8 @@ int main() {
   for(auto &t: threads){
     t.join();
   }
-  assert(std::is_sorted(items.begin(), items.end()) && items.size() == WORKERS / 2);
+  assert(items.size() == WORKERS / 2);
+  assert(std::is_sorted(items.begin(), items.end()));
   std::cout << "Test passed!" << std::endl;
 
   return 0;
